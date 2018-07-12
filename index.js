ac1694baec2fcaaf3acb8784bc37b68e6da86bd0
cross <cross@vgg.ru>
2018-07-12 15:26:27 +0300

    Add arithmeticService module

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello, World!");
+import ArithmeticService from './src/arithmeticService';
+
+let a = 16, b = 2;
+let service = new ArithmeticService();
+
+console.log("a = ", a);
+console.log("b = ", b);
+console.log("a + b = ", service.sum(a, b));
+console.log("a - b = ", service.difference(a, b));
+console.log("a * b = ", service.multiplication(a, b));
+console.log("a / b = ", service.division(a, b));
+console.log("a ^ b = ", service.power(a, b));
+console.log("Square root of a equals ", service.squareRoot(a));
