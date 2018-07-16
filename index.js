2495c80121ab47ec9c91498286e266c1c1c14a33
cross <cross@vgg.ru>
2018-07-16 20:47:02 +0300

    Add Image component

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Image from './src/Image';
+
+import products from './src/constants/Products';
+const product = products[0];
+
+ReactDOM.render(
+    <Image src={product.imageUrl} width='100px' height='100px' alt={product.title} />,
+    document.getElementById('root')
+);
