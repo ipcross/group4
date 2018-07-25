175a3a346fe7685324a751a771721b05e46e4b6d
cross <cross@vgg.ru>
2018-07-25 16:02:04 +0300

    Make images local

--- src/constants/Products.js
+++ src/constants/Products.js
@@ -1,27 +1,29 @@
+import images from './../../web/img/*.png'
+
 const products = [
     {
         id: 1,
         title: 'Harry Potter and the Philosopher\'s Stone',
         price: 300,
-        imageUrl: 'https://best-wallpaper.net/wallpaper/1600x900/1106/Emma-Watson-in-Harry-Potter-7_1600x900.jpg'
+        imageUrl: images['1']
     },
     {
         id: 2,
         title: 'Harry Potter and the Chamber of Secrets',
         price: 350,
-        imageUrl: 'http://www.loseternautas.com/wp-content/uploads/2011/07/Harry-Potter-and-the-Deathly-Hallows-Part-II_1366x768.jpg'
+        imageUrl: images['2']
     },
     {
         id: 3,
         title: 'Harry Potter and the Prisoner of Azkaban',
         price: 330,
-        imageUrl: 'https://www.10wallpaper.com/wallpaper/1920x1080/1108/Harry_Potter_7_-_Professor_Snape_Wallpaper_1920x1080.jpg'
+        imageUrl: images['3']
     },
     {
         id: 4,
         title: 'Harry Potter and the Goblet of Fire',
         price: 450,
-        imageUrl: 'http://licreatura.com/wp-content/uploads/2016/06/harry-potter-wallpaper-22.jpg'
+        imageUrl: images['4']
     }
 ];
 
