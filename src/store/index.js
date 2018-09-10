4daf392e7e1207a30a1888c5570208eaf27f191b
cross <cross@vgg.ru>
2018-09-10 19:50:57 +0300

    Create store and connect middleware

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from '~/src/reducers';
+
+
+const store = createStore(
+    combineReducers(reducers),
+    applyMiddleware(thunk)
+);
+
+export default store;
