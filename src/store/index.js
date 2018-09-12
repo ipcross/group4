ed6c02228500d242c81c5857113ec3e94e0f7227
cross <cross@vgg.ru>
2018-09-12 14:06:42 +0300

    Add redux enhancer

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
-import { combineReducers, createStore, applyMiddleware } from 'redux';
+import {
+    combineReducers,
+    createStore,
+    applyMiddleware,
+    compose
+} from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '~/src/reducers';
 
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
     combineReducers(reducers),
-    applyMiddleware(thunk)
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
 );
 
 export default store;
