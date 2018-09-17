bc49e6a0c1b61dd4f51a78e0984a0e248355abe6
cross <cross@vgg.ru>
2018-09-17 19:30:04 +0300

    Prepare store for SSR

--- src/store/index.js
+++ src/store/index.js
@@ -12,12 +12,25 @@ import { initializeCart } from '~/src/actions/cart';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-    combineReducers(reducers),
-    composeEnhancers(
-        applyMiddleware(...middlewares)
-    )
-);
+let store;
+
+if (__SERVER__) {
+    store = createStore(
+        combineReducers(reducers),
+        compose(
+            applyMiddleware(...middlewares)
+        )
+    );
+} else {
+    store = createStore(
+        combineReducers(reducers),
+        window.__INITIAL_STATE__,
+        composeEnhancers(
+            applyMiddleware(...middlewares)
+        )
+    );
+    delete window.__INITIAL_STATE__;
+}
 
 store.dispatch(initializeCart());
 
